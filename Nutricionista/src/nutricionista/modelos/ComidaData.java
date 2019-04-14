fe191c937a614d81d674f08f74e5792b278c6975
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2019-04-13 21:46:56 -0300

    Agrego metodos de crear, borrar, buscar y modificar en ComidaData

--- Nutricionista/src/nutricionista/modelos/ComidaData.java
+++ Nutricionista/src/nutricionista/modelos/ComidaData.java
@@ -13,6 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -30,6 +31,111 @@ public class ComidaData {
             System.out.println("Error al abrir al obtener la conexion");
         }
     }
+    
+     //metodo que recibe una comida con su nombre, cantidad de calorias, un detalle y lo inserta en la base de datos
+    public void guardarComida(Comida comida){ 
+        try {
+            //en los values del string se usan ? para representar los valores reales que van a llevar
+            //dinamicamente estan ordenados, el primer ? es nombre, el segundo es calorias y el tercero es detalle
+            String sql = "INSERT INTO comida (nombre, calorias, detalle) VALUES ( ? , ? , ? );";
+
+            //al ps le decimos valores van en los values ?
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, comida.getNombre());
+            ps.setDouble(2, comida.getCalorias() ); 
+            ps.setString(3, comida.getDetalle());
+            
+            ps.executeUpdate();//sentencia que cuando se ejecuta hace el insert sobre la tabla alumnos
+            
+            ResultSet rs = ps.getGeneratedKeys(); //obtengo las claves 
+
+            if (rs.next()) { //seteo id
+                comida.setId(rs.getInt(1));
+            } else {
+                System.out.println("No se pudo obtener el id luego de insertar una comida");
+            }
+            ps.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar una comida: " + ex.getMessage());
+        }
+    }
+    
+    //metodo que recibe un id de una comida y hace un delete en la base de datos y la borra
+    public void borrarComida(int id){
+    try {
+            
+            String sql = "DELETE FROM comida WHERE idComida =?;";
+
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id);
+                      
+            ps.executeUpdate();//hace executeUpdate porque estamos haciendo un delete
+                        
+            ps.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+        }
+        
+    
+    }
+    
+    
+    
+    public void actualizarComida(Comida comida){
+    
+        try {
+            
+            String sql = "UPDATE comida SET nombre = ?, calorias = ? , "
+                    + "detalle =? WHERE idComida = ?;";
+
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, comida.getNombre());
+            ps.setDouble(2, comida.getCalorias() ); 
+            ps.setString(3, comida.getDetalle());
+            ps.setInt(4, comida.getId());
+            ps.executeUpdate();//hace executeUpdate porque estamos haciendo un update
+            
+          
+            ps.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+        }
+    
+    }
+    
+    //metodo que busca una comida unica a partir de un id
+    public Comida buscarComida(int id){
+    Comida comida=null;
+    try {
+            
+            String sql = "SELECT * FROM comida WHERE idComida =?;";
+
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id);
+           
+            
+            ResultSet resultSet=ps.executeQuery(); //hace executeQuery porque estamos haciendo un Select
+            
+            while(resultSet.next()){
+                comida = new Comida();
+                comida.setId(resultSet.getInt("idComida"));
+                comida.setNombre(resultSet.getString("nombre"));
+                comida.setCalorias(resultSet.getDouble("calorias"));
+                comida.setDetalle(resultSet.getString("detalle"));
+                             
+            }      
+            ps.close();
+    
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+        }
+        
+        return comida;
+    }
+    
     
     //metodo que devuelve lista de comidas cuyas calorias son menores
     //al parametro indicado
